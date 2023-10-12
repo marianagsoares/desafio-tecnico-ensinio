6ec62b5887c94379ba1c6013d846d2b6a8361c68
marianagsoares <marianasoares.ti@gmail.com>
2023-10-11 22:08:59 -0300

    feat: add CT03

--- cypress/e2e/internacionalizacao.cy.js
+++ cypress/e2e/internacionalizacao.cy.js
@@ -1,15 +1,23 @@
-describe('Internacionalização', () => {
+describe("Internacionalização", () => {
   beforeEach(() => {
-    cy.visit('https://desafio-ensinio.vercel.app/');
+    cy.visit("https://desafio-ensinio.vercel.app/");
+  });
+  
+  it("CT02: Modificar idioma da tela institucional para inglês", () => {
+    cy.getByDataTestId("languageselect-label").should("be.visible");
+    cy.getByDataTestId("languageselect-label").trigger("mouseover");
+
+    cy.getByDataTestId("languageselect-item").contains("EN").click();
+
+    cy.contains("All in one platform");
   });
 
-  it('CT02: Modificar idioma da tela institucional para inglês', () => {
-      cy.getByDataTestId('languageselect-label').should('be.visible');
+  it("CT03: Modificar idioma da tela institucional para espanhol", () => {
+    cy.getByDataTestId("languageselect-label").should("be.visible");
+    cy.getByDataTestId("languageselect-label").trigger("mouseover");
 
-      cy.getByDataTestId('languageselect-label').trigger('mouseover');
-      
-      cy.getByDataTestId('languageselect-item').contains('EN').click();
-      
-      cy.contains('All in one platform'); 
+    cy.getByDataTestId("languageselect-item").contains("ES").click();
+
+    cy.contains("Todo en una plataforma");
   });
 });
