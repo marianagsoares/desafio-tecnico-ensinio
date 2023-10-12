b53700dbbec38b5b4f50c16bd8f7fdbe004d0192
marianagsoares <marianasoares.ti@gmail.com>
2023-10-11 22:16:07 -0300

    feat: add failing CT01

--- cypress/e2e/internacionalizacao.cy.js
+++ cypress/e2e/internacionalizacao.cy.js
@@ -2,7 +2,16 @@ describe("Internacionalização", () => {
   beforeEach(() => {
     cy.visit("https://desafio-ensinio.vercel.app/");
   });
-  
+
+  it("CT01: Modificar idioma da tela institucional para português", () => {
+    cy.getByDataTestId("languageselect-label").should("be.visible");
+    cy.getByDataTestId("languageselect-label").trigger("mouseover");
+
+    cy.getByDataTestId("languageselect-item").contains("PT").click();
+
+    cy.contains("Tudo em uma plataforma");
+  });
+
   it("CT02: Modificar idioma da tela institucional para inglês", () => {
     cy.getByDataTestId("languageselect-label").should("be.visible");
     cy.getByDataTestId("languageselect-label").trigger("mouseover");
